f576e1f8bf6d03a336f63d04c6ba0db2c2f479ab
Pratyush-Kumar-Jaiswal <pratyush.kumar2506@gmail.com>
2023-04-11 23:40:02 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCeQ5--QDCeQQ7V9V1HNt8YpBCDVyEJFVY",
+  authDomain: "genz-cab7b.firebaseapp.com",
+  projectId: "genz-cab7b",
+  storageBucket: "genz-cab7b.appspot.com",
+  messagingSenderId: "496923158641",
+  appId: "1:496923158641:web:79a2fd03bdc1a030b1d3d0",
+  measurementId: "G-J8NRWBB8DQ"
+};
